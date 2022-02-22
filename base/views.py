42dda21950c3bf59c4ddfe9b0298041260e4e496
Sergey Khizhnyak <khizhnyak56843@gmail.com>
2022-02-22 16:06:59 +0300

    Сохранение перед созданием user model

--- base/views.py
+++ base/views.py
@@ -98,7 +98,7 @@ def userProfile(request, pk):
     user = User.objects.get(id=pk)
     rooms = user.room_set.all()
     room_messages = user.message_set.all()
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     context = {'user': user, 'rooms': rooms, 
                'room_messages': room_messages, 'topics': topics}
     return render(request, 'base/profile.html', context)
